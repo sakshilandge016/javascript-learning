c87b755a8db2bebc14566dfced64237619e2c8fb
sakshilandge016 <sakshilandge016@gmail.com>
2024-11-10 20:31:21 +0530

    01-variable assignment
    01-variable assignment

---
+++ 01-variable-assignment.js
@@ -0,0 +1,14 @@
+var City = "Nagpur"
+console.log("City:", City);
+
+var BestFriend = "Prashant"
+console.log("Best Friend:", BestFriend);
+
+var Country = "India"
+console.log("Country:", Country);
+
+var SchoolName = "Prabhodhankar Thakre"
+console.log("School Name:", SchoolName);
+
+var CollegeName = "Raisoni College"
+console.log("College Name:", CollegeName);
---
+++ 01-variableAssign.js
@@ -0,0 +1,35 @@
+var myFullName;
+console.log("Before Initialization", myFullName );
+
+myFullName = "Sakshi Ashok Landge";
+console.log("myFullName:", myFullName );
+
+myName = "Sakshi Landge";
+console.log("myName:", myName);
+
+var firstName = "Sakshi"
+console.log("firstName:", firstName);
+
+var middleName = "Ashok"
+console.log("middleName:", middleName);
+
+var lastName = "Landge"
+console.log("lastName:", lastName);
+
+var myLove = "I love only JS"
+console.log("myLove:", myLove);
+
+var age = "24"
+console.log("age:", age);
+
+var address = "Navneet Nagar"
+console.log("address:", address);
+
+var pinCode = "440023"
+console.log("pinCode:", pinCode);
+
+var gender = "Female"
+console.log("gender:", gender);
+
+var isMarried = "Unmarried"
+console.log("isMarried:", isMarried);

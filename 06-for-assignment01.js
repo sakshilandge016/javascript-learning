bfa2c6ab5ef59bcc8be64f926f1205c54b34d566
sakshilandge016 <sakshilandge016@gmail.com>
2024-11-19 19:59:48 +0530

    Create 06-for-assignment01.js

---
+++ 06-for-assignment01.js
@@ -0,0 +1,37 @@
+console.log("1)---5 to 15 increment");
+var i = 5;
+while (i<=15) {
+    console.log(i);
+    i++;
+
+}
+
+console.log("2)---50 to 40 decrementing");
+for (var index = 50; index >= 40; index--) {
+    console.log(index);
+}
+
+console.log("3)---first 15 odd numbers");
+for (let index = 1; index < 30; index=index+2) {
+    console.log(index);
+}
+
+console.log("4)---first 10 even numbers");
+for (let index = 2; index <= 20; index=index+2) {
+    console.log(index);
+}
+
+console.log("5)---Table of 5");
+for (var index = 5; index <= 50; index=index+5) {
+    console.log(index);
+}
+
+console.log("6)---Table of 10");
+for (var index = 10; index <= 100; index=index+10) {
+    console.log(index);
+}
+
+console.log("7)---Table of 10 in reverse oder");
+for (var index = 100; index >= 10; index=index-10) {
+    console.log(index);
+}
